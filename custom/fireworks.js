2b2a0afc7ff53114e88e9bcfb34204e9eeabeb8a
Jonriel <iamkirito50@gmail.com>
2023-10-13 09:03:47 +0800

    Final

--- custom/fireworks.js
+++ custom/fireworks.js
@@ -1,54 +1,54 @@
-const btn = document.getElementById('btn');
+// const btn = document.getElementById('btn');
 
-btn.addEventListener('click', () => {
-    const particles = [];
-    const color = randomColor();
+// btn.addEventListener('click', () => {
+//     const particles = [];
+//     const color = randomColor();
     
-    const particle = document.createElement('span');
-    particle.classList.add('particle', 'move');
+//     const particle = document.createElement('span');
+//     particle.classList.add('particle', 'move');
     
-    const { x, y } = randomLocation();
-    particle.style.setProperty('--x', x);
-    particle.style.setProperty('--y', y);
-    particle.style.background = color;
+//     const { x, y } = randomLocation();
+//     particle.style.setProperty('--x', x);
+//     particle.style.setProperty('--y', y);
+//     particle.style.background = color;
     
-    btn.appendChild(particle);
+//     btn.appendChild(particle);
     
-    particles.push(particle);
+//     particles.push(particle);
     
-    setTimeout(() => {
+//     setTimeout(() => {
     
-        for(let i = 0; i < 100; i++) {
-            const innerP = document.createElement('span');
-            innerP.classList.add('particle', 'move');
-            innerP.style.transform = `translate(${x}, ${y})`;
+//         for(let i = 0; i < 100; i++) {
+//             const innerP = document.createElement('span');
+//             innerP.classList.add('particle', 'move');
+//             innerP.style.transform = `translate(${x}, ${y})`;
 
-            const xs = Math.random() * 200 - 100 + 'px';
-            const ys = Math.random() * 200 - 100 + 'px';
-            innerP.style.setProperty('--x', `calc(${x} + ${xs})`);
-            innerP.style.setProperty('--y', `calc(${y} + ${ys})`);
-            innerP.style.animationDuration = Math.random() * 300 + 200 + 'ms';
-            innerP.style.background = color;
+//             const xs = Math.random() * 200 - 100 + 'px';
+//             const ys = Math.random() * 200 - 100 + 'px';
+//             innerP.style.setProperty('--x', `calc(${x} + ${xs})`);
+//             innerP.style.setProperty('--y', `calc(${y} + ${ys})`);
+//             innerP.style.animationDuration = Math.random() * 300 + 200 + 'ms';
+//             innerP.style.background = color;
             
-            btn.appendChild(innerP);
-            particles.push(innerP)
-        }
+//             btn.appendChild(innerP);
+//             particles.push(innerP)
+//         }
         
-        setTimeout(() => {
-            particles.forEach(particle => {
-                particle.remove();
-            })
-        }, 1000)
-    }, 1000);
-});
+//         setTimeout(() => {
+//             particles.forEach(particle => {
+//                 particle.remove();
+//             })
+//         }, 1000)
+//     }, 1000);
+// });
 
-function randomLocation() {
-    return {
-        x: Math.random() * window.innerWidth - window.innerWidth / 2 + 'px',
-        y: Math.random() * window.innerHeight - window.innerHeight / 2 + 'px',
-    }
-}
+// function randomLocation() {
+//     return {
+//         x: Math.random() * window.innerWidth - window.innerWidth / 2 + 'px',
+//         y: Math.random() * window.innerHeight - window.innerHeight / 2 + 'px',
+//     }
+// }
 
-function randomColor() {
-    return `hsl(${Math.floor(Math.random() * 361)}, 100%, 50%)`;
-}
+// function randomColor() {
+//     return `hsl(${Math.floor(Math.random() * 361)}, 100%, 50%)`;
+// }
